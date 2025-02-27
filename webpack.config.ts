0e5e35dd542140898b6903115753a64e16179a51
Taha Arab <81683966+senFouka@users.noreply.github.com>
2025-02-28 02:50:18 +0330

    Initialize renovate/eslint-webpack-plugin-5.x

---
+++ webpack.config.ts
@@ -0,0 +1,46 @@
+import path from "path";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import ESLintPlugin from "eslint-webpack-plugin";
+import {TsconfigPathsPlugin} from "tsconfig-paths-webpack-plugin";
+import packageJson from "./package.json" with {type: "json"};
+
+const webpackConfig = (env): webpack.Configuration => ({
+    entry: "./src/index.tsx",
+    ...(env.production || !env.development ? {} : {devtool: "eval-source-map"}),
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+        plugins: [new TsconfigPathsPlugin()]
+    },
+    output: {
+        path: path.join(import.meta.dirname, "/dist"),
+        filename: "build.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                options: {
+                    transpileOnly: true
+                },
+                exclude: /dist/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        }),
+        new webpack.DefinePlugin({
+            "process.env.PRODUCTION": env.production || !env.development,
+            "process.env.NAME": JSON.stringify(packageJson.name),
+            "process.env.VERSION": JSON.stringify(packageJson.version)
+        }),
+        new ForkTsCheckerWebpackPlugin(),
+        new ESLintPlugin({files: "./src/**/*.{ts,tsx,js,jsx}"})
+    ]
+});
+
+export default webpackConfig;
